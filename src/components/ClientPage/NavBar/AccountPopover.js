1a7ace85666489f429b7acf540af632525d49d57
xaviSalazar <xavicoel@gmail.com>
2022-10-11 20:30:38 +0200

    added page client

---
+++ src/components/ClientPage/NavBar/AccountPopover.js
@@ -0,0 +1,129 @@
+import { useRef, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+// @mui
+import { alpha } from '@mui/material/styles';
+import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton } from '@mui/material';
+// components
+import MenuPopover from '../../MenuPopover';
+// mocks_
+// import account from '../../../_mock/account';
+import { httpManager } from '../../../managers/httpManager';
+
+import { useNavigate } from 'react-router-dom';
+
+// ----------------------------------------------------------------------
+
+
+const MENU_OPTIONS = [
+  {
+    label: 'Home',
+    icon: 'eva:home-fill',
+    linkTo: '/',
+  },
+  {
+    label: 'Profile',
+    icon: 'eva:person-fill',
+    linkTo: '#',
+  },
+  {
+    label: 'Settings',
+    icon: 'eva:settings-2-fill',
+    linkTo: '#',
+  },
+];
+
+// ----------------------------------------------------------------------
+
+export default function AccountPopover({account}) {
+  const anchorRef = useRef(null);
+
+  const [open, setOpen] = useState(null);
+  const navigate = useNavigate();
+
+
+  const handleOpen = (event) => {
+    setOpen(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setOpen(null);
+  };
+
+  const handleLogout = async () => {
+
+    const token = localStorage.getItem('customerToken');
+    const config = {
+        headers: {Authorization: `Bearer ${token}`}
+    }
+    const loggingout = await httpManager.logoutUser(config)
+
+    if(loggingout['data']['responseCode'] === 200) {
+      navigate('/') 
+    }
+  }
+
+  return (
+    <>
+      <IconButton
+        ref={anchorRef}
+        onClick={handleOpen}
+        sx={{
+          p: 0,
+          ...(open && {
+            '&:before': {
+              zIndex: 1,
+              content: "''",
+              width: '100%',
+              height: '100%',
+              borderRadius: '50%',
+              position: 'absolute',
+              bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
+            },
+          }),
+        }}
+      >
+        <Avatar src={account.image_url} alt="photoURL" />
+      </IconButton>
+
+      <MenuPopover
+        open={Boolean(open)}
+        anchorEl={open}
+        onClose={handleClose}
+        sx={{
+          p: 0,
+          mt: 1.5,
+          ml: 0.75,
+          '& .MuiMenuItem-root': {
+            typography: 'body2',
+            borderRadius: 0.75,
+          },
+        }}
+      >
+        <Box sx={{ my: 1.5, px: 2.5 }}>
+          <Typography variant="subtitle2" noWrap>
+            {account.name}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+            {account.email}
+          </Typography>
+        </Box>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <Stack sx={{ p: 1 }}>
+          {MENU_OPTIONS.map((option) => (
+            <MenuItem key={option.label} to={option.linkTo} component={RouterLink} onClick={handleClose}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Stack>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <MenuItem onClick={handleLogout} sx={{ m: 1 }}>
+          Cerrar sesion
+        </MenuItem>
+      </MenuPopover>
+    </>
+  );
+}
--- src/pages/ClientPage.js
+++ src/pages/ClientPage.js
@@ -1,48 +1,78 @@
 import { Link as RouterLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 // @mui
+import { Outlet } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import { Button, Typography, Container, Box } from '@mui/material';
 // components
 import Page from '../components/Page';
+import ClientNavbar from '../components/ClientPage/NavBar'
+import ClientSidebar from '../components/ClientPage/SiderBar';
+import { httpManager } from '../managers/httpManager';
 
-// ----------------------------------------------------------------------
 
-const ContentStyle = styled('div')(({ theme }) => ({
-  maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
+const APP_BAR_MOBILE = 64;
+const APP_BAR_DESKTOP = 92;
+
+const RootStyle = styled('div')({
   display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  padding: theme.spacing(12, 0)
+  minHeight: '100%',
+  overflow: 'hidden'
+});
+
+const MainStyle = styled('div')(({ theme }) => ({
+  flexGrow: 1,
+  overflow: 'auto',
+  minHeight: '100%',
+  paddingTop: APP_BAR_MOBILE + 24,
+  paddingBottom: theme.spacing(10),
+  [theme.breakpoints.up('lg')]: {
+    paddingTop: APP_BAR_DESKTOP + 24,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2)
+  }
 }));
 
 // ----------------------------------------------------------------------
 
 export default function ClientPage() {
+
+  const [open, setOpen] = useState(false);
+  const [account, setAccount] = useState("")
+
+  useEffect(() => {
+
+    console.log(`Client page useEffect`)
+    const token = localStorage.getItem('customerToken')
+    console.log(token)
+    const config = {
+          headers: {Authorization: `Bearer ${token}`}
+    }
+
+    const authenticate = async () => {
+      return await httpManager.customerAuth(config)
+    }
+    
+    authenticate()
+    .then(response => {if(response['data']['responseCode'] === 200)
+
+    console.log(`authenticated`)
+    setAccount(response['data']['responseData'])
+    console.log(response['data']['responseData'])
+    })
+  }, [])
+
+
+  
   return (
-    <Page title="Client page">
-      <Container>
-        <ContentStyle sx={{ textAlign: 'center', alignItems: 'center' }}>
-          <Typography variant="h3" paragraph>
-          Pagina CLIENTE
-          </Typography>
 
-          <Typography sx={{ color: 'text.secondary' }}>
-           Pagina CLIENTE
-          </Typography>
-
-          <Box
-            component="img"
-            src="/static/illustrations/illustration_404.svg"
-            sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
-          />
-
-          <Button to="/" size="large" variant="contained" component={RouterLink}>
-            Go to Home
-          </Button>
-        </ContentStyle>
-      </Container>
-    </Page>
+    <RootStyle>
+      <ClientNavbar onOpenSidebar={() => setOpen(true)} account={account}/>
+      <ClientSidebar isOpenSidebar={open} onCloseSidebar={() => setOpen(false)} account = {account}/>
+      <MainStyle>
+        <Outlet />
+      </MainStyle>
+    </RootStyle>
+   
   );
 }
