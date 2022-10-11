29e7aa3d3d5540054d372cc9d2094ca890af857d
xaviSalazar <xavicoel@gmail.com>
2022-10-11 23:48:57 +0200

    template edition

--- src/components/Templates/TemplatesListTable.js
+++ src/components/Templates/TemplatesListTable.js
@@ -2,6 +2,12 @@ import { useState, useEffect } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+//-----------------------product template viewer
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+import { Container, Typography } from '@mui/material';
 import {
   Box,
   Card,
@@ -11,32 +17,98 @@ import {
   TableHead,
   TablePagination,
   TableRow,
-  Button
+  Button,
+  TextField
 } from '@mui/material';
 
+const styles = {
+
+  top: 28,
+  right: 0,
+  left: 0,
+  zIndex: 1,
+  p: 1,
+  bgcolor: 'background.paper',
+};
+
+const EcommerceShop=() => {
+  const docs = [
+    { uri: "https://d1d5i0xjsb5dtw.cloudfront.net/AUTORIZACION+MODIFICABLE+.docx"} 
+  ];
+
+  return (
+            <DocViewer  
+                pluginRenderers={DocViewerRenderers}
+                documents={docs} 
+                style={{ height: 700 }}
+            />
+          )
+}
+
+const docList =[
+  {
+    id: 1,
+    name: 'Autorizacion_ejemplo',
+    category: 'Derecho Civil',
+    campos: ['nombres_completos', 
+    'nacionalidad', 'estado_civil',
+     'domicilio', 'telefono', 
+     'correo_electronico', 'persona_poder',
+    'persona_recibe_poder', 'cedula_poder', 
+    'estado_civil_poder','numero_celular']
+  },
+  {
+    id: 2,
+    name: 'Acta de desarrollo',
+    category: 'Derecho Civil',
+    campos: ['nombres_completos', 
+    'nacionalidad', 'estado_civil',
+     'domicilio', 'telefono', 
+     'correo_electronico', 'persona_poder',
+    'persona_recibe_poder', 'cedula_poder', 
+    'estado_civil_poder','numero_celular']
+  },
+  {
+    id: 3,
+    name: 'Notaria pdf',
+    category: 'Derecho Civil',
+    campos: ['nombres_completos', 
+    'nacionalidad', 'estado_civil',
+     'domicilio', 'telefono', 
+     'correo_electronico', 'persona_poder',
+    'persona_recibe_poder', 'cedula_poder', 
+    'estado_civil_poder','numero_celular']
+  }
+]
+
+
 export const TemplatesListTable = ({ ...rest }) => {
+  const [open, setOpen] = useState(false);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(5);
+  const [singleDocument, setSingleDocument] = useState([])
 
-    const docList =[
-      {
-        id: 1,
-        name: 'Autorizacion_ejemplo',
-        category: 'Derecho Civil'
-      },
-      {
-        id: 2,
-        name: 'Acta de desarrollo',
-        category: 'Derecho Civil'
-      },
-      {
-        id: 3,
-        name: 'Notaria pdf',
-        category: 'Derecho Civil'
-      }
-    ]
+  
+  const handleClick = (e, id) => {
+    setOpen((prev) => !prev);
+    const doc = docList.filter(item => item.id === id )
+    setSingleDocument(doc)
+    console.log(doc)
+    console.log(id)
+  };
 
-    const [page, setPage] = useState(0);
-    const [limit, setLimit] = useState(5);
+  const handleClickAway = () => {
+    setOpen(false);
+  };
 
+ 
+
+    const champsToFillbyUser = singleDocument[0] && singleDocument[0].campos.map(
+      (item, index) => {
+        return <TextField key={index} required id="outlined-required" label={item} defaultValue=""/>
+      })
+
+   
 
     const handleLimitChange = (event) => {
       setLimit(event.target.value);
@@ -49,7 +121,21 @@ export const TemplatesListTable = ({ ...rest }) => {
 
 
 return (
+  <ClickAwayListener onClickAway={handleClickAway}>
+
+{open ? (
+          <Box sx={styles}>
+            <Button variant="contained" startIcon={<ModeEditOutlineOutlinedIcon />}>
+              Editar con mis datos
+            </Button>
+            {singleDocument[0] && champsToFillbyUser}
+            <EcommerceShop />
+            
+          </Box>
+        ) : 
     <Card {...rest}>
+      
+     
         <PerfectScrollbar>
         <Box sx={{ flexFlow: '1', overflowX: "scroll",}}>
           <Table >
@@ -130,20 +216,25 @@ return (
                     {`notas a agregar`}
                   </TableCell>
                   <TableCell>
+                    <Stack direction="row" spacing={1}>
+                    {/* <IconButton >
                     <ModeEditOutlineOutlinedIcon/>
+                    </IconButton> */}
+                    <IconButton onClick={e => handleClick(e, document.id)}>
                     <VisibilityOutlinedIcon />
+                    </IconButton>
+                    </Stack>
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
+           
           </Table>
         </Box>
+       
       </PerfectScrollbar>
-
-
-
-
-
+     
+    
         <TablePagination
         component="div"
         count={docList.length}
@@ -153,7 +244,12 @@ return (
         rowsPerPage={limit}
         rowsPerPageOptions={[5, 10, 25]}
         />
-    </Card>
+      
+      
+       
+    </Card>}
+     </ClickAwayListener>
+    
 )
 
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -45,108 +45,108 @@
 //   );
 //  }
 
-// import React, { useState } from "react";
-// import { Logger } from "logging-library";
-// import FileViewer from "react-file-viewer";
-// import { CustomErrorComponent } from "custom-error";
-// import DocViewer from "react-doc-viewer";
-// import Docxtemplater from 'docxtemplater';
-// import PizZip from 'pizzip';
-// import PizZipUtils from 'pizzip/utils/index.js';
-// import { saveAs } from 'file-saver';
+import React, { useState } from "react";
+import { Logger } from "logging-library";
+import FileViewer from "react-file-viewer";
+import { CustomErrorComponent } from "custom-error";
+import DocViewer from "react-doc-viewer";
+import Docxtemplater from 'docxtemplater';
+import PizZip from 'pizzip';
+import PizZipUtils from 'pizzip/utils/index.js';
+import { saveAs } from 'file-saver';
 
 
-// const EcommerceShop = () => {
-//   function loadFile(url, callback) {
-//     PizZipUtils.getBinaryContent(url, callback);
-//   }
+const EcommerceShop = () => {
+  function loadFile(url, callback) {
+    PizZipUtils.getBinaryContent(url, callback);
+  }
   
-//   const generateDocument = () => {
-//     loadFile(
-//       // 'https://docxtemplater.com/tag-example.docx',
-//       'https://d1d5i0xjsb5dtw.cloudfront.net/AUTORIZACION+MODIFICABLE+.docx',
-//       function (error, content) {
-//         if (error) {
-//           throw error;
-//         }
-//         var zip = new PizZip(content);
-//         var doc = new Docxtemplater(zip, {
-//           paragraphLoop: true,
-//           linebreaks: true,
-//         });
-//         doc.setData({
-//           first_name: 'Simón Bolívar',
-//           last_name: 'Doña',
-//           phone: '0652455478',
-//           description: 'New Website',
-//         });
-//         try {
-//           // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-//           doc.render();
-//         } catch (error) {
-//           // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-//           function replaceErrors(key, value) {
-//             if (value instanceof Error) {
-//               return Object.getOwnPropertyNames(value).reduce(function (
-//                 error,
-//                 key
-//               ) {
-//                 error[key] = value[key];
-//                 return error;
-//               },
-//               {});
-//             }
-//             return value;
-//           }
-//           console.log(JSON.stringify({ error: error }, replaceErrors));
+  const generateDocument = () => {
+    loadFile(
+      // 'https://docxtemplater.com/tag-example.docx',
+      'https://d1d5i0xjsb5dtw.cloudfront.net/AUTORIZACION+MODIFICABLE+.docx',
+      function (error, content) {
+        if (error) {
+          throw error;
+        }
+        var zip = new PizZip(content);
+        var doc = new Docxtemplater(zip, {
+          paragraphLoop: true,
+          linebreaks: true,
+        });
+        doc.setData({
+          first_name: 'Simón Bolívar',
+          last_name: 'Doña',
+          phone: '0652455478',
+          description: 'New Website',
+        });
+        try {
+          // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+          doc.render();
+        } catch (error) {
+          // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+          function replaceErrors(key, value) {
+            if (value instanceof Error) {
+              return Object.getOwnPropertyNames(value).reduce(function (
+                error,
+                key
+              ) {
+                error[key] = value[key];
+                return error;
+              },
+              {});
+            }
+            return value;
+          }
+          console.log(JSON.stringify({ error: error }, replaceErrors));
 
-//           if (error.properties && error.properties.errors instanceof Array) {
-//             const errorMessages = error.properties.errors
-//               .map(function (error) {
-//                 return error.properties.explanation;
-//               })
-//               .join('\n');
-//             console.log('errorMessages', errorMessages);
-//             // errorMessages is a humanly readable message looking like this :
-//             // 'The tag beginning with "foobar" is unopened'
-//           }
-//           throw error;
-//         }
-//         var out = doc.getZip().generate({
-//           type: 'blob',
-//           mimeType:
-//             'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-//         }); //Output the document using Data-URI
-//         saveAs(out, 'output.docx');
-//       }
-//     );
-//   };
+          if (error.properties && error.properties.errors instanceof Array) {
+            const errorMessages = error.properties.errors
+              .map(function (error) {
+                return error.properties.explanation;
+              })
+              .join('\n');
+            console.log('errorMessages', errorMessages);
+            // errorMessages is a humanly readable message looking like this :
+            // 'The tag beginning with "foobar" is unopened'
+          }
+          throw error;
+        }
+        var out = doc.getZip().generate({
+          type: 'blob',
+          mimeType:
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        }); //Output the document using Data-URI
+        saveAs(out, 'output.docx');
+      }
+    );
+  };
 
-//   return (
-//     <div className="p-2">
-//       <button onClick={generateDocument}>Generate document</button>
-//     </div>
-//   );
-// };
-
-// export default EcommerceShop;
-
-import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
-import { Container, Stack, Typography } from '@mui/material';
-
-
-const EcommerceShop=() => {
-  const docs = [
-    { uri: "https://d1d5i0xjsb5dtw.cloudfront.net/AUTORIZACION+MODIFICABLE+.docx"} 
-  ];
-
-  return (<Container>
-            <DocViewer  
-                pluginRenderers={DocViewerRenderers}
-                documents={docs} 
-                style={{ height: 700 }}
-            />
-          </Container>)
-}
+  return (
+    <div className="p-2">
+      <button onClick={generateDocument}>Generate document</button>
+    </div>
+  );
+};
 
 export default EcommerceShop;
+
+// import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+// import { Container, Stack, Typography } from '@mui/material';
+
+
+// const EcommerceShop=() => {
+//   const docs = [
+//     { uri: "https://d1d5i0xjsb5dtw.cloudfront.net/AUTORIZACION+MODIFICABLE+.docx"} 
+//   ];
+
+//   return (<Container>
+//             <DocViewer  
+//                 pluginRenderers={DocViewerRenderers}
+//                 documents={docs} 
+//                 style={{ height: 700 }}
+//             />
+//           </Container>)
+// }
+
+// export default EcommerceShop;
